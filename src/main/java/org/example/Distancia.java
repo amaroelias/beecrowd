8ea94fce980919d71b853294de353f8efa8df398
amaroelias <amaronascimento2016@hotmail.com>
2025-07-24 23:32:26 -0300

    distancia resolvido

--- src/main/java/org/example/Distancia.java
+++ src/main/java/org/example/Distancia.java
@@ -1,2 +1,11 @@
+package org.example;
+
+import java.util.Scanner;
+
 public class Distancia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num * 2+" minutos");
+    }
 }
